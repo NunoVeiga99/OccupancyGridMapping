9fec62d1f32598c395efaba47c31405d4410988c
andre99amaral <45856606+andre99amaral@users.noreply.github.com>
2020-10-29 15:59:36 +0000

    Log_to_Prob Function
    vai transformar os valores do occupancy grid mapping que estão em logaritmo em probabilidades

--- sa_project/src/ogm_mapping/scripts/mapping.py
+++ sa_project/src/ogm_mapping/scripts/mapping.py
@@ -10,3 +10,10 @@ def callback(msg):
 rospy.init_node('scan_values')
 sub = rospy.Subscriber('/iris_0/scan', LaserScan, callback)
 rospy.spin()
+
+# The Inverse Range Sensor Model will return a matrix with Logaritmic Values and we have to put that values as Proabilities
+# p(m|z,x) = 1-(1/(1+exp{log_odds}))
+def log_to_prob(matrix)
+    exp_logodds = np.exp(matrix)
+    probability = 1- np.divide(1,1+exp_logodds)
+    return probability
