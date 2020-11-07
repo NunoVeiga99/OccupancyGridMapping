acfea7703e803598b9fe8e31865797fc3475902a
andre99amaral <45856606+andre99amaral@users.noreply.github.com>
2020-11-07 10:33:47 +0000

    Update mapping.py
    Meti o algoritmo occupancy grid mapping, tenham atenção a cruiação do inverse range sensor function, tem de se chamar inverse_range_sensor_model e retornar as celulas, as quais no OGM chamei de translacão[i] ( i = 0, x   i = 1, y)

--- sa_project/src/ogm_mapping/scripts/mapping.py
+++ sa_project/src/ogm_mapping/scripts/mapping.py
@@ -8,15 +8,15 @@ from sensor_msgs.msg import LaserScan
 from geometry_msgs.msg import PoseStamped
 from tf.transformations import euler_from_quaternion
 
-n_depth = 400 #n de celulas
+n_height = 400 #n de celulas
 n_width = 400 #n de celulas
 resolution = 0.05 #resolucao em metros
 min_angle = -np.pi # em radianos
 max_angle = np.pi #em radianos
-#height = ???
+#depth = ???
 
 #Mapa com as probabilidades
-map = np.zeros((n_depth,n_width))
+map = np.zeros((n_height,n_width))
 angle = np.linspace(min_angle, max_angle, 161)#TO DO:Confirmar 161 sera o n de beams do laser
 
 def lidar_callback(msg):
@@ -39,11 +39,55 @@ poseSub = rospy.Subscriber('/mavros/local_position/pose',PoseStamped, pose_callb
 rospy.spin()
 
 
-
-
 # The Inverse Range Sensor Model will return a matrix with Logaritmic Values and we have to put that values as Proabilities
 # p(m|z,x) = 1-(1/(1+exp{log_odds}))
 def log_to_prob(matrix):
     exp_logodds = np.exp(matrix)
     probability = 1- np.divide(1,1+exp_logodds)
     return probability
+
+# Lets define a mapping with Occupancy Grid Mapping
+def map_with_OGM(rotation, translation, n_height, n_width, resolution, map, x_0, y_0)
+    # First let's peparare auxiliar variables
+    # 1) rotation about Z-axis more known as yaw
+    euler_obtained_from_quaternion = euler_from_quaternion(rotation)
+    yaw = euler_obtained_from_quaternion[2]
+    # 2) Defining x and y positions
+    positionx = translation[0]
+    positiony = translation[1]
+    
+    # 2) Using a python and ROS nav_msgs::msg::_OccupancyGrid 
+    OGM = OccupancyGrid()
+    # Set up this function about the header
+    OGM_.header.stamp = rospy.Time.now()
+    OGM.header.frame_id = "map"
+    OGM.header.seq = 0
+    # Set up this function about the info
+    OGM.info.resolution = resolution
+    OGM.info.width = n_width
+    OGM.info.height = n_height
+    OGM.info.map_load_time = rospy.Time.now()
+    # Set up the map origin
+    OGM.info.origin.position.x = x_0
+    OGM.info.origin.position.y = y_0
+    
+    # Let's now write the OGM algorithm to use in the
+    if len(lidar_points) != 0
+        # If the vector it's not empty we should update the matrix with the help of the inverse range sensor model
+        # l(t,i) = l(t-1,i) + inverse range-sensor model - l0 (l0 = 0)
+        # Note: the l matriz was defined in the inverse ange sensor code with enteries like  positionx, positiony, theta, lidar_points
+        IRS_Matrix = np.add(IRS_Matrix, inverse_range_sensor_mode(positionx, positiony, yaw, lidar_points))
+        # Converter de logaritmo para probabilidade
+        Prob_Matrix = log_to_prob(IRS_Matriz)
+        # Colocar em 1 dimensão a matriz
+        Prob_Matrix_1D = np.concatenate(Prob_Matrix, axis = 0)
+        # Guardar as probabilidades no OGM definido em cima
+        for i in range(len(Prob_Matrix_1D))
+            OGM.data.append(Prob_Matrix_1D[i] * 100) # Multiply by 100 to obtain a 0-100% probabilities
+    
+    # Publish a topic with the map
+     OGM_publisher = rospy.Publisher('/map_new', OccupancyGrid, queue_size=1)
+     OGM_publisher.publish(OGM)
+                                           
+# Dúvidas: onde vamos buscar translação para depois termos posição x e y
+#          qual a posição inicial x e y
